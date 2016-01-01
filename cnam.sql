5356c3b8ec23933ec444885533d0cfcae6e603a8
JJ <jj@liberation.systems>
2016-01-01 16:24:55 -0500

    added basic cnam schema. This will likely change as it's quite janky.

---
+++ cnam.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS `cnam`;
+CREATE TABLE `cnam` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `npa` int(3) NOT NULL,
+  `nxx` int(3) NOT NULL,
+  `xxxx` int(4) NOT NULL,
+  `cnam` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
